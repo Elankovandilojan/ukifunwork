d39ba8a2a7483f2f07df1eee0cb2eedfb69a6dd8
elankvandilojan <dilojan36@gmail.com>
2019-06-06 16:11:38 +0530

    new

---
+++ script.js
@@ -0,0 +1,79 @@
+SC.initialize({
+  client_id: 'a3e059563d7fd3372b49b37f00a00bcf'
+
+});
+
+$(document).ready(function() {
+
+  SC.stream('/tracks/615320520', function(sound) {
+    $('#start1').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop1').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+  SC.stream('/tracks/495538437', function(sound) {
+    $('#start2').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop2').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+  SC.stream('/tracks/602054046', function(sound) {
+    $('#start3').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop3').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+  SC.stream('/tracks/571882998', function(sound) {
+    $('#start4').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop4').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+  SC.stream('/tracks/214260066', function(sound) {
+    $('#start5').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop5').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+  SC.stream('/tracks/191214159', function(sound) {
+    $('#start6').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#stop6').click(function(e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+
+
+
+
+
+
+});
